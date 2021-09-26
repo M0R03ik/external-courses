58c246fd4503209d533d4244e249ffd8bdd1fd7f
M0R03ik <ya.moroz95@yandex.ru>
2021-09-26 19:18:16 +0300

    "Added task-05.js"

---
+++ src/ex2_js-basics-part2/task-05.js
@@ -0,0 +1,11 @@
+function getMaxElem(arr) {
+  let max = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+module.exports = getMaxElem;
