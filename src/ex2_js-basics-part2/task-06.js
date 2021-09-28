992bcf573dd9aa86e6fcabb1479cd168cb434849
M0R03ik <ya.moroz95@yandex.ru>
2021-09-28 20:09:01 +0300

    "Added early return. task-06"

--- src/ex2_js-basics-part2/task-06.js
+++ src/ex2_js-basics-part2/task-06.js
@@ -2,16 +2,16 @@ function isSimple(num) {
   let count = 0;
   let message = `Число ${num} - простое число`;
   if (num > 1000) {
-    message = 'Данные неверны';
-  } else if (num === 0 || num === 1) {
-    message = 'Не причисляется ни к простым, ни к составным числам';
-  } else {
-    for (let i = 2; i <= num; i += 1) {
-      if (num % i === 0 && count < 2) {
-        count += 1;
-      }
+    return 'Данные неверны';
+  } if (num === 0 || num === 1) {
+    return 'Не причисляется ни к простым, ни к составным числам';
+  }
+  for (let i = 2; i <= num; i += 1) {
+    if (num % i === 0 && count < 2) {
+      count += 1;
     }
   }
+
   if (count > 1) {
     message = `Число ${num} - составное число`;
   }
