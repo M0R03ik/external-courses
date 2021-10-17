1df3a23f5396b3d33d54425d22d0aae63a8688c0
M0R03ik <ya.moroz95@yandex.ru>
2021-10-17 16:33:25 +0300

    Добавил пробелы между блоками кода

--- src/ex2_js-basics-part2/task-06.js
+++ src/ex2_js-basics-part2/task-06.js
@@ -1,11 +1,15 @@
 function isSimple(num) {
   let count = 0;
   let message = `Число ${num} - простое число`;
+
   if (num > 1000) {
     return 'Данные неверны';
-  } if (num === 0 || num === 1) {
+  }
+
+  if (num === 0 || num === 1) {
     return 'Не причисляется ни к простым, ни к составным числам';
   }
+
   for (let i = 2; i <= num; i += 1) {
     if (num % i === 0 && count < 2) {
       count += 1;
@@ -15,6 +19,7 @@ function isSimple(num) {
   if (count > 1) {
     message = `Число ${num} - составное число`;
   }
+
   return message;
 }
 
