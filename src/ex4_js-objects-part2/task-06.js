4d4cbbb5c0fd11f725e9b30fbc7a8ffff717aad4
M0R03ik <ya.moroz95@yandex.ru>
2021-10-17 15:05:08 +0300

    Сделал сразу return

--- src/ex4_js-objects-part2/task-06.js
+++ src/ex4_js-objects-part2/task-06.js
@@ -1,6 +1,5 @@
 function toCamelCase(str) {
-  const arr = str.split(' ');
-  return arr.map((word) => word[0].toUpperCase() + word.slice(1)).join(' ');
+  return str.split(' ').map((word) => word[0].toUpperCase() + word.slice(1)).join(' ');
 }
 
 module.exports = toCamelCase;
