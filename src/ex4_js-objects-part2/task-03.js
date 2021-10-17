3a872ecf20799c3693a28b0587740386098141b7
M0R03ik <ya.moroz95@yandex.ru>
2021-10-17 14:58:28 +0300

    Добавил проверку на наличие пробелов

--- src/ex4_js-objects-part2/task-03.js
+++ src/ex4_js-objects-part2/task-03.js
@@ -1,5 +1,17 @@
 function removeSpaces(str) {
-  return str.slice(1, -1);
+  if (str[0] === ' ' && str[str.length - 1] === ' ') {
+    return str.slice(1, -1);
+  }
+
+  if (str[0] === ' ') {
+    return str.slice(1);
+  }
+
+  if (str[str.length - 1] === ' ') {
+    return str.slice(0, -1);
+  }
+
+  return str;
 }
 
 module.exports = removeSpaces;
